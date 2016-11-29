4feda8e1a715081784324ceddce9ff354df4197c
haurbano <h.a.u.r1993@gmail.com>
2016-11-29 10:36:27 -0500

    Commit con cambios

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ var fs = require('fs');
 var request = require('request');
 var async = require('async');
 
-function downloadImage() {
-    request('https://yt3.ggpht.com/-Yjh5ftUJb4A/AAAAAAAAAAI/AAAAAAAAAAA/LKsf6_8IS5w/s900-c-k-no-mo-rj-c0xffffff/photo.jpg')
+function downloadImage(url) {
+    request(url)
         .pipe(fs.createWriteStream('images/haur.jpg'));
+
     console.log("Image download...");
 }
 
+var url = 'https://yt3.ggpht.com/-Yjh5ftUJb4A/AAAAAAAAAAI/AAAAAAAAAAA/LKsf6_8IS5w/s900-c-k-no-mo-rj-c0xffffff/photo.jpg';
 
-async.parallel(downloadImage(),function () {
+async.parallel(downloadImage(url),function () {
   console.log("image downloaded");
 });
 
